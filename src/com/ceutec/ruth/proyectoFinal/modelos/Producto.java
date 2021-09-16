db624e66e7270d6a272b59dfa0feacdc14e7f785
fabiolagos <fabionicolailagospadilla@gmail.com>
2021-09-15 21:37:52 -0600

    Comentarios en código

--- src/com/ceutec/ruth/proyectoFinal/modelos/Producto.java
+++ src/com/ceutec/ruth/proyectoFinal/modelos/Producto.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ceutec.ruth.proyectoFinal.modelos;
 
+// Clase que se usará como modelo (tipo de dato) en listas.
 public class Producto {
     public int codigo = 0;
     public String nombre = "";
