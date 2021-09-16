d3f800b42083e4f039a98fd0693559ba3861f812
fabiolagos <fabionicolailagospadilla@gmail.com>
2021-09-15 20:20:27 -0600

    Mejoras

--- src/com/ceutec/ruth/proyectoFinal/Citta.java
+++ src/com/ceutec/ruth/proyectoFinal/Citta.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ceutec.ruth.proyectoFinal;
 
 import com.ceutec.ruth.proyectoFinal.datos.BaseDeDatos;
